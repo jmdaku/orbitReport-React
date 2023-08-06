dde42e2e0894a40c7477d7d64343aca65d546ff5
jmdaku <jmdaku@gmail.com>
2023-08-06 14:33:43 -0400

    added banner and table

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+
+
+
+function Banner() {
+
+    return(
+        <>
+            <h1>Banner</h1> 
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    );
+}
+
+
+export default Banner;
