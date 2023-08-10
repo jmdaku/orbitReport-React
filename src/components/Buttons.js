f6c1313b58c4837fb2ae8a2701d76907b84217b0
jmdaku <jmdaku@gmail.com>
2023-08-09 20:04:58 -0400

    updated buttons & added table

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,27 @@
+import satData from "./components/satData.js";
+import App from "../App.js";
+import { filterByType , setSat , displaySats } from "../App.js";
+
+
+
+
 const Buttons = () => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+         return (
+           <button onClick={() => filterByType(sat)} key={id}>
+             {sat} Orbit
+           </button>
+        );
+      })}
+
+
+      <button onClick={() => {
+        setSat(satData)}
+      }
+      >All Orbits </button>
+  </div>
   );
 };
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,22 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
+        {sat.map((id, data) => {
+        <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational}</td>//"create a conditional that will tell a user if a satellite is active or inactive based on its operational value."
         </tr>
+        }
+        )};
         </tbody>
       </table>
   );
